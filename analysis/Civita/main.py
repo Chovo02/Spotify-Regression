1e895cfe46525acca0663587a394118a5461ce77
Matteo Civita <M.civita@itsrizzoli.it>
2023-06-29 14:31:15 +0200

    Funzioni per la visualizzazione dei dati

--- analysis/Civita/main.py
+++ analysis/Civita/main.py
@@ -9,18 +9,18 @@ def feat(x):
 
 df = pd.read_csv("data/SpotifySongPolularityAPIExtract.csv", low_memory=False)
 
+def matrix(df):
+    df["feat"] = df["track_name"].apply(lambda x: feat(x))
 
+    df_matrix = df.drop(["track_id", "track_name", "artist_name"], axis=1)
+    px.scatter_matrix(df_matrix.sample(1000)).show()
 
-df["feat"] = df["track_name"].apply(lambda x: feat(x))
+def artist_scatter(df):
+    df.sort_values("artist_name", ascending=False, inplace=True)
+    name_dict={}
+    for i, name in enumerate(df["artist_name"].unique()):
+        name_dict[name] = i
 
-df_matrix = df.drop(["track_id", "track_name", "artist_name"], axis=1)
-px.scatter_matrix(df_matrix.sample(1000)).show()
-
-df.sort_values("artist_name", ascending=False, inplace=True)
-name_dict={}
-for i, name in enumerate(df["artist_name"].unique()):
-    name_dict[name] = i
-
-df_artist_scatter = df
-df_artist_scatter["artist_name"] = df_artist_scatter["artist_name"].map(name_dict)
-px.scatter(df_artist_scatter, x="artist_name", y="popularity", color="feat").show()
+    df_artist_scatter = df
+    df_artist_scatter["artist_name"] = df_artist_scatter["artist_name"].map(name_dict)
+    px.scatter(df_artist_scatter, x="artist_name", y="popularity", color="feat").show()
