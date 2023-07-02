c6f945f8b8080ec37d6ae44d6eb037b089ce2198
Mattia Rossini <m.rossini@itsrizzoli.it>
2023-07-02 13:57:26 +0200

    Aggiunti fit_transform e ArtistPopularityTransformer

--- ml-model/SpotifyCustomTransformer.py
+++ ml-model/SpotifyCustomTransformer.py
@@ -1,7 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.pipeline import Pipeline
 from featbyid import feat_dict
-import pandas as pd
 
 class FeatTransformer(BaseEstimator, TransformerMixin):
         
@@ -20,5 +18,24 @@ class FeatTransformer(BaseEstimator, TransformerMixin):
             feats_averaged[key] = sum(pop_list) / len(pop_list) if pop_list else 0
         
         X["feats_avg_popularity"] = X["track_id"].map(feats_averaged)
-        X.drop("popularity", axis=1, inplace=True)
         return X
+    
+    def fit_transform(self, X, y=None):
+        self.fit(X, y)
+        return self.transform(X, y)
+    
+class ArtistPopularityTransformer(BaseEstimator, TransformerMixin):
+        
+    def fit(self, X, y=None):
+        self.avg_popularity = X.groupby('artist_name')['popularity'].mean().to_dict()
+        return self
+
+    def transform(self, X, y=None):
+        X['artist_name'] = X['artist_name'].map(self.avg_popularity)
+        X = X.drop(['track_id', 'track_name'], axis=1)
+        return X
+    
+    def fit_transform(self, X, y=None):
+        self.fit(X, y)
+        return self.transform(X, y)
+
--- ml-model/main.py
+++ ml-model/main.py
@@ -1,31 +1,17 @@
 import pandas as pd
-import json
+from SpotifyCustomTransformer import FeatTransformer, ArtistPopularityTransformer
+from sklearn.pipeline import Pipeline
 
-def calculate_average_popularity(df, artist_col, popularity_col):
-    avg_popularity = df.groupby(artist_col)[popularity_col].mean().to_dict()
-    return avg_popularity
-
-def average_popularity_for_features(avg_artist_pop, feats_json):
-    feats_averaged = {}
-
-    for key, artists in feats_json.items():
-        pop_list = [avg_artist_pop.get(artist, 0) for artist in artists]
-
-        feats_averaged[key] = sum(pop_list) / len(pop_list) if pop_list else 0
-
-    return feats_averaged
 
 df = pd.read_csv("data\\SpotifySongPolularityAPIExtract.csv")
 df.drop_duplicates(subset=['track_id'], keep='first', inplace=True)
-avg_artist_pop = calculate_average_popularity(df, "artist_name", "popularity")
 
-with open("data\\feats.json","r") as f:
-    feats_json = json.load(f)
+X = df.drop(["popularity"], axis=1)
+y = df["popularity"]
 
-feats_averaged = average_popularity_for_features(avg_artist_pop, feats_json)
+pipeline = Pipeline([
+    ("feat_transformer", FeatTransformer()),
+    ("artist_popularity_transformer", ArtistPopularityTransformer())
+])
 
-df["feats_avg_popularity"] = df["track_id"].map(feats_averaged)
-
-print(df.head())
-
-
+print(pipeline.fit_transform(X, y).head()) 
