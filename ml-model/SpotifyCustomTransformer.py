11536bb8557855fe2d63d4a214f8882a2a9406f2
Matteo Civita <M.civita@itsrizzoli.it>
2023-07-04 10:57:15 +0200

    Fix bug transformers

--- ml-model/SpotifyCustomTransformer.py
+++ ml-model/SpotifyCustomTransformer.py
@@ -4,20 +4,24 @@ from featbyid import feat_dict
 class FeatTransformer(BaseEstimator, TransformerMixin):
         
     def fit(self, X, y=None):
-        self._feat = feat_dict(X)
         self._y = y
+        X["popularity"] = self._y
+        self._avg_popularity = X.groupby("artist_name")["popularity"].mean().to_dict()
+        X.drop("popularity", axis=1, inplace=True)
+        return self
     
     def transform(self, X, y=None):
         X["popularity"] = self._y
-        avg_popularity = X.groupby("artist_name")["popularity"].mean().to_dict()
+        self._feat = feat_dict(X)
         feats_averaged = {}
 
         for key, artists in self._feat.items():
-            pop_list = [avg_popularity.get(artist, 0) for artist in artists]
+            pop_list = [self._avg_popularity.get(artist, 0) for artist in artists]
 
             feats_averaged[key] = sum(pop_list) / len(pop_list) if pop_list else 0
         
         X["feats_avg_popularity"] = X["track_id"].map(feats_averaged)
+        X.drop("popularity", axis=1, inplace=True)
         return X
     
     def fit_transform(self, X, y=None):
@@ -27,12 +31,15 @@ class FeatTransformer(BaseEstimator, TransformerMixin):
 class ArtistPopularityTransformer(BaseEstimator, TransformerMixin):
         
     def fit(self, X, y=None):
-        self.avg_popularity = X.groupby('artist_name')['popularity'].mean().to_dict()
+        self._y = y
+        X["popularity"] = self._y
+        self._avg_popularity = X.groupby("artist_name")["popularity"].mean().to_dict()
+        X.drop("popularity", axis=1, inplace=True)
         return self
 
     def transform(self, X, y=None):
-        X['artist_name'] = X['artist_name'].map(self.avg_popularity)
-        X = X.drop(['track_id', 'track_name'], axis=1)
+        X["artist_name"] = X["artist_name"].map(self._avg_popularity)
+        X = X.drop(["track_id", "track_name"], axis=1)
         return X
     
     def fit_transform(self, X, y=None):
