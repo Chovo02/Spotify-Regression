ec4451323786a2c48975448f6d305e6ac5578944
Matteo Civita <M.civita@itsrizzoli.it>
2023-07-05 12:09:31 +0200

    Eliminazione colonna market da transformer

--- ml-model/SpotifyCustomTransformer.py
+++ ml-model/SpotifyCustomTransformer.py
@@ -9,7 +9,6 @@ class FeatTransformer(BaseEstimator, TransformerMixin):
         self.y = y
         X["popularity"] = self.y
         self.avg_popularity = X.groupby("artist_name")["popularity"].mean().to_dict()
-        X.drop("popularity", axis=1, inplace=True)
         return self
     
     def transform(self, X, y=None):
@@ -20,10 +19,7 @@ class FeatTransformer(BaseEstimator, TransformerMixin):
         for key, items in self.feat.items():
             pop_list = [self.avg_popularity.get(artist, 0) for artist in items["feats"]]
             feats_averaged[key] = sum(pop_list) / len(pop_list) if pop_list else 0
-            market_list[key] = items["markets"]
         X["feats_avg_popularity"] = X["track_id"].map(feats_averaged)
-        X["markets"] = X["track_id"].map(market_list)
-        X.drop("popularity", axis=1, inplace=True)
         return X
     
     def fit_transform(self, X, y=None):
@@ -36,7 +32,6 @@ class ArtistPopularityTransformer(BaseEstimator, TransformerMixin):
         self.y = y
         X["popularity"] = self.y
         self.avg_popularity = X.groupby("artist_name")["popularity"].mean().to_dict()
-        X.drop("popularity", axis=1, inplace=True)
         return self
 
     def transform(self, X, y=None):
